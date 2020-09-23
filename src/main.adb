fe885be056a106fc0f2f0fc4b0aa41dd4bcbf234
mcxbain <mcxbain@126.com>
2020-09-23 03:25:39 +0200

    commit

--- src/main.adb
+++ src/main.adb
@@ -1,32 +1,13 @@
 with Ada.Text_IO;
-with Ada.Strings.Unbounded;
 With Adx.App.Run;
-With Loophole;
 With Adx.Lib.Sysout;
-with Spark;
-with System;
-With Ada.Directories;
 
 use Ada.Text_IO;
-use Ada.Strings.Unbounded;
 
 procedure Main is
 
 begin
 
---Adx.App.Run.Main;
-
--- Testtin Committing
---put_line(To_String(Adx.Lib.Sysout.Sysout("ls")));
-
-
---Loophole.Run;
-
-spark.my_run;
-
-
-
-
-
+Adx.App.Run.Main;
 
 end Main;
