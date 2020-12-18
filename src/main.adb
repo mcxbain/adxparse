16366d18c812aa8ea942addf9bef43f8ad8c161d
mcxbain <mcxbain@126.com>
2020-12-18 07:47:44 +0100

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -7,10 +7,7 @@
 --                                 B o d y                                  --
 --                                                                          --
 ------------------------------------------------------------------------------
-with Ada.Text_IO;
 With Adx.Terminal;
-
-use Ada.Text_IO;
 
 procedure Main is
 
